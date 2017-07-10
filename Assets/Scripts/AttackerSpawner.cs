f1b25d75344e29dced010e26b5a8f644f5a2bfdc
Nimrod Dayan <nimrod@codepond.org>
2017-07-10 15:43:59 +0300

    Spawn attacker randomly every 3 seconds

---
+++ Assets/Scripts/AttackerSpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackerSpawner : MonoBehaviour {
+    public float spawnDelaySeconds;
+    public GameObject attackerPrefab;
+    private GameObject attackersParent;
+    // Use this for initialization
+    void Start () {
+        attackersParent = new GameObject("Attackers");
+        InvokeRepeating("ShouldSpawnAttacker", spawnDelaySeconds, spawnDelaySeconds);
+    }
+    
+    void ShouldSpawnAttacker()
+    {
+        if (Random.value > 0.8f)
+        {
+            var attacker = Instantiate(attackerPrefab, transform.position, Quaternion.identity);
+            attacker.transform.parent = attackersParent.transform;
+        }
+    }
+    // Update is called once per frame
+    void Update () {
+        
+    }
+}
