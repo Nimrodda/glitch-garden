0169e6b26197a460565cd29df47f4d1c02f019c3
Nimrod Dayan <nimrod@codepond.org>
2017-07-10 22:38:40 +0300

    Added star currency

--- Assets/Scripts/Defender.cs
+++ Assets/Scripts/Defender.cs
@@ -2,33 +2,5 @@
 
 public class Defender : MonoBehaviour
 {
-    Vector3 pos = new Vector3();
-
-    void Start ()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update ()
-    {
-    }
-
-    void OnTriggerEnter2D(Collider2D other)
-    {
-
-    }
-
-    void OnMouseDrag()
-    {
-        /*pos.x = Input.mousePosition.x;
-        pos.y = Input.mousePosition.y;
-        pos.z = Input.mousePosition.z;
-        Vector3 worldpos = Camera.main.ScreenToWorldPoint(pos);
-        worldpos.x = Mathf.Clamp(Mathf.Round(worldpos.x), 1f, 9f);
-        worldpos.y = Mathf.Clamp(Mathf.Round(worldpos.y), 1f, 5f);
-        worldpos.z = 0;
-        print(worldpos);
-        gameObject.transform.position = worldpos;*/
-    }
+    public int cost;
 }
---
+++ Assets/Scripts/Star.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Star : MonoBehaviour 
+{
+    public int value;
+    private Animator animator;
+    void Start()
+    {
+        animator = transform.parent.transform.parent.GetComponent<Animator>();
+    }
+
+    void OnMouseDown()
+    {
+        // TODO: Add up to total stars
+        animator.SetTrigger("Picked");
+    }
+}
