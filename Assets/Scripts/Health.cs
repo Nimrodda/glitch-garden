d4bb7366db54de2cd1892da426297a11d72183c3
Nimrod Dayan <nimrod@codepond.org>
2017-07-12 12:10:26 +0300

    Added health bar to defenders and attackers

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -2,24 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Health : MonoBehaviour {
+public class Health : MonoBehaviour 
+{
+    public GameObject healthbarPrefab;
     public float health;
-    // Use this for initialization
-    void Start () {
-        
+
+    private RectTransform healthbar;
+    private float healthbarWidth;
+    private float initialHealth;
+
+    void Start () 
+    {
+        var hb = Instantiate(healthbarPrefab, transform.position, Quaternion.identity);
+        hb.GetComponent<RectTransform>().SetParent(transform);
+        healthbar = hb.transform.Find("Bar").GetComponent<RectTransform>();
+        healthbarWidth = healthbar.sizeDelta.x;	
+        initialHealth = health;
     }
     
-    // Update is called once per frame
-    void Update () {
-        
-    }
-
     public void Damage(float hit)
     {
         health -= hit;
+        updateHealthbar();
         if (health <= 0)
         {
             Destroy(gameObject);
         }
+    }
+
+    private void updateHealthbar()
+    {
+        float y = healthbar.sizeDelta.y;
+        healthbar.sizeDelta = new Vector2(healthbarWidth / initialHealth * health, y);
     }
 }
