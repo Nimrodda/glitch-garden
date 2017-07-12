6d67232f12148ace65416a515496905421f5848f
Nimrod Dayan <nimrod@codepond.org>
2017-07-12 16:46:16 +0300

    Added win/lose messages

--- Assets/Scripts/LevelTimer.cs
+++ Assets/Scripts/LevelTimer.cs
@@ -7,15 +7,21 @@ public class LevelTimer : MonoBehaviour
     private Text timeRemainingText;
     private Slider slider;
     private Timer timer;
-    
+    private Text winText;
+
     void Start() 
     {
         timeRemainingText = transform.Find("TimeRemaining").GetComponent<Text>();
+        winText = transform.Find("Win Text").GetComponent<Text>();
         slider = transform.Find("Slider").GetComponent<Slider>();
         timer = GetComponent<Timer>();
         timer.Tick += (timeRemaining) => {
             slider.value = 1f / (float) timer.timerSeconds * timeRemaining;
             timeRemainingText.text = timeRemaining.ToString();
+        };
+        timer.Finish += () => {
+            winText.enabled = true;
+            Time.timeScale = 0;
         };
         timer.Run();
     }
---
+++ Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoseCollider : MonoBehaviour 
+{
+    private bool lost;
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.GetComponent<Attacker>())
+        {
+            if (!lost)
+            {
+                lost = true;
+                transform.Find("Lose Text").GetComponent<Text>().enabled = true;
+                Time.timeScale = 0;
+            }
+        }
+    }
+}
